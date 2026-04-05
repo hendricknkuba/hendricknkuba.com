bf4cb1ac11c65460d996d56b1db96bd730a993b6
hendricknkuba <imhendrick16@gmail.com>
2026-04-04 21:55:15 -0700

    Add ESLint configuration for TypeScript with recommended rules

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/ts/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'no-console': 'warn',
+    },
+  },
+];
